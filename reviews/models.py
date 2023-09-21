8c8273756e34f049da0be5971094ed62010bc374
pozhara <poliazhara04@gmail.com>
2023-09-21 13:05:15 +0000

    Add review app, review model and review serializer

---
+++ reviews/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+STARS = [
+    ('1', 1),
+    ('1.5', 1.5),
+    ('2', 2),
+    ('2.5', 2.5),
+    ('3', 3),
+    ('3.5', 3.5),
+    ('4', 4),
+    ('4.5', 4.5),
+    ('5', 5)
+]
+
+GENRE = [
+    ('Sandbox', 'Sandbox'),
+    ('Real-time strategy', 'Real-time strategy'),
+    ('Shooters', 'Shooters'),
+    ('Multiplayer online battle arena', 'Multiplayer online battle arena'),
+    ('Role-playing', 'Role-playing'),
+    ('Simulation and sports', 'Simulation and sports'),
+    ('Action adventure', 'Action adventure'),
+    ('Survival', 'Survival'),
+    ('Horror', 'Horror')
+]
+
+DIFFICULTY = [
+    ('Easy', 'Easy'),
+    ('Medium', 'Medium'),
+    ('Hard', 'Hard'),
+    ('Extreme', 'Extreme'),
+    ('Nightmare', 'Nightmare')
+]
+
+AGE = [
+    ('3', 3),
+    ('7', 7),
+    ('12', 12),
+    ('16', 16),
+    ('18', 18),
+]
+
+
+class Review(models.Model):
+    """
+    Review model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    stars = models.CharField(max_length=3, choices=STARS)
+    genre = models.CharField(max_length=100, choices=GENRE)
+    developed_by = models.CharField(max_length=255)
+    level_of_difficulty = models.CharField(max_length=50, choices=DIFFICULTY)
+    suitable_age = models.CharField(max_length=15, choices=AGE)
+    hours_spent = models.IntegerField(max_length=300, blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_g1aaa6.jpg', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
---
+++ reviews/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger tham 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Review
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'stars', 'genre', 'developed_by',
+            'level_of_difficulty', 'suitable_age', 'hours_spent', 'image'
+        ]

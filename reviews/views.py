d1528d6e1f026e7b917a3d4242251810f3803a0f
pozhara <poliazhara04@gmail.com>
2023-09-21 13:43:24 +0000

    Add ReviewDetail view to retrieve, edit and delete reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Review
 from .serializers import ReviewSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ReviewList(APIView):
@@ -26,3 +28,40 @@ class ReviewList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReviewDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReviewSerializer
+
+    def get_object(self, pk):
+        try:
+            review = Review.objects.get(pk=pk)
+            self.check_object_permissions(self.request, review)
+            return review
+        except Review.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(
+            review, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(
+            review, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        review = self.get_object(pk)
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

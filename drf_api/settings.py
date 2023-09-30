c89e3189f09398b8fdce17ddbe27b2eff63671c2
pozhara <poliazhara04@gmail.com>
2023-09-30 11:04:44 +0000

    Change allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'arcade-slate-api.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
 
 # Application definition

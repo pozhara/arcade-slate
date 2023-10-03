2fe7433b44a56013fa74871d80532655bd2e7bc3
pozhara <poliazhara04@gmail.com>
2023-10-03 10:14:28 +0000

    Add refreshing access token and logged in status conditional rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import styles from './App.module.css'
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
-import { Route, Switch } from 'react-router-dom';
-import './api/axiosDefaults'
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
     <div className={styles.App}>
-      <NavBar/>
+      <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path='/' render={()=><h1>Home page</h1>}/>
-          <Route exact path='/reviews' render={()=><h1>Reviews</h1>}/>
-          <Route exact path='/deals' render={()=><h1>Deals</h1>}/>
-          <Route exact path='/signin' render={()=><SignInForm/>}/>
-          <Route exact path='/signup' render={()=><SignUpForm/>}/>
-          <Route render={()=><p>Page not found!</p>}/>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,33 +1,121 @@
 import React from "react";
-import styles from "../styles/NavBar.module.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top" variant="dark">
-      <Container className="text-white">
-        <NavLink to='/'>
-          <Navbar.Brand>arcade slate</Navbar.Brand>
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            arcade slate
+          </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {currentUser}
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/'>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink to='/reviews' className={styles.NavLink} activeClassName={styles.Active}>
-              <i class="fa-solid fa-receipt"></i>Reviews
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/reviews"
+            >
+              <i className="fa-solid fa-receipt"></i>Reviews
             </NavLink>
-            <NavLink to='/deals' className={styles.NavLink} activeClassName={styles.Active}>
-              <i class="fa-solid fa-cart-arrow-down"></i>Deals
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/deals"
+            >
+              <i className="fa-solid fa-cart-shopping"></i>Deals
             </NavLink>
-            <NavLink to='/signin' className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fas fa-sign-in-alt"></i>Sign in
-            </NavLink>
-            <NavLink to='/signup' className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fas fa-user-plus"></i>Sign up
-            </NavLink>
+
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
